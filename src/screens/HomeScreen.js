6c6925d3188a6282f34b8db57f8d6bd11b9c9a02
Syket Das <80612737+syket-das@users.noreply.github.com>
2022-02-03 22:50:45 +0530

    resolved git error

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -22,7 +22,6 @@ const SCREEN_WIDTH = Dimensions.get('window').width;
 export default function HomeScreen({ navigation }) {
   const [delivery, setDelivery] = React.useState(true);
   const [indexCheck, setIndexCheck] = React.useState('0');
-  // hello
   return (
     <View style={styles.container}>
       <HomeHeader navigation={navigation} />
